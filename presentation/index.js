d074e23aec90c59a3a5572921569bee65d51f546
Jen Luker <jluker@deseretdigital.com>
2018-05-17 19:41:30 -0600

    Remove references to company specific slides

--- presentation/index.js
+++ presentation/index.js
@@ -1,10 +1,10 @@
 // Import React
 import React from "react";
-import { AboutMe, Formidable } from './aboutme';
-import { A11yIntro, WhatIsA11y, WhatIsDisability, TypesOfDisability, Why, WhyBusiness1, WhyBusiness2, WhyBusiness3 } from './aboutA11y';
-import { GettingStartedIntro, Axe, DesignTools, DevTools } from './gettingStarted';
-import { TestYourSiteIntro, Ksl, Utah, DesNews } from './testYourSite';
-import { BreakdownIntro, Checklists, StartNow, ByComponent, ByPage } from './breakdown';
+import { AboutMe, Formidable } from "./aboutme";
+import { A11yIntro, WhatIsA11y, WhatIsDisability, TypesOfDisability, Why, WhyBusiness1, WhyBusiness2, WhyBusiness3 } from "./aboutA11y";
+import { GettingStartedIntro, Axe, DesignTools, DevTools } from "./gettingStarted";
+import { TestYourSiteIntro } from "./testYourSite";
+import { BreakdownIntro, Checklists, StartNow, ByComponent, ByPage } from "./breakdown";
 
 // Import Spectacle Core tags
 import {
@@ -25,80 +25,80 @@ import {
 } from "spectacle";
 
 // Import image preloader util
-import preloader from 'spectacle/lib/utils/preloader';
-const images = {
-  JenDevDesk: require('../assets/aboutme/JensDesk.jpg'),
-  JenDeskColor: require('../assets/aboutme/JensDeskColor.jpg'),
-	
-}
-preloader(images);
+import preloader from "spectacle/lib/utils/preloader";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
-import styles from './styles';
+import styles from "./styles";
+const images = {
+  JenDevDesk: require("../assets/aboutme/JensDesk.jpg"),
+  JenDeskColor: require("../assets/aboutme/JensDeskColor.jpg")
+
+};
+preloader(images);
 
 // Require CSS
 require("normalize.css");
 
 const themeColors = {
-	primary: '#FAFAFA',
-	secondary: '#191919',
-	tertiary: '#981804',
-	dktertiary: 'deepskyblue',
-	quartenary: '#FAFAFA',
-	faded: '#CCCCCC',
-	background: 'linear-gradient(#F8FAFA, #D8DADA)',
+  primary: "#FAFAFA",
+  secondary: "#191919",
+  tertiary: "#981804",
+  dktertiary: "deepskyblue",
+  quartenary: "#FAFAFA",
+  faded: "#CCCCCC",
+  background: "linear-gradient(#F8FAFA, #D8DADA)"
 };
 
 const theme = createTheme(themeColors, {
-	primary: 'Raleway',
+  primary: "Raleway"
 });
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-          {/* Title Screen */}
-				<Slide
-					bgColor="faded"
-					bgImage={images.JenDevDesk}
-					bgDarken={0.7}
-				>
-					<Notes>
+        {/* Title Screen */}
+        <Slide
+          bgColor="faded"
+          bgImage={images.JenDevDesk}
+          bgDarken={0.7}
+        >
+          <Notes>
 						As speakers, we do our best, but sometimes slides are
 						just too difficult to read. You can follow along at
 						a11y-sprint-filler.surge.sh
-					</Notes>
-					<Layout style={styles.flexCol}>
-						<Fill style={styles.spacePlease}>
-							<Heading
-								caps
-								size={1}
-								textColor="primary"
-								style={{ textShadow: '1px 1px' + ' 6px #111' }}
-							>
-								Accessibility
-							</Heading>
+          </Notes>
+          <Layout style={styles.flexCol}>
+            <Fill style={styles.spacePlease}>
               <Heading
-								margin="10px 0 0"
-								textColor="dktertiary"
-								size={3}
-								bold
-								style={{ textShadow: '1px 1px' + ' 6px #111' }}
-							>
+                caps
+                size={1}
+                textColor="primary"
+                style={{ textShadow: "1px 1px" + " 6px #111" }}
+              >
+								Accessibility
+              </Heading>
+              <Heading
+                margin="10px 0 0"
+                textColor="dktertiary"
+                size={3}
+                bold
+                style={{ textShadow: "1px 1px" + " 6px #111" }}
+              >
 								the sprint-filler way
-							</Heading>
-						</Fill>
-						<Fill style={styles.spacePlease}>
-							<Link
-								textColor="dktertiary"
-								style={styles.smallText}
-								href="http://a11y-sprint-filler.surge.sh"
-							>
+              </Heading>
+            </Fill>
+            <Fill style={styles.spacePlease}>
+              <Link
+                textColor="dktertiary"
+                style={styles.smallText}
+                href="http://a11y-sprint-filler.surge.sh"
+              >
 								Follow along with the slides:
 								http://a11y-sprint-filler.surge.sh
-							</Link>
-							{/* <br />
+              </Link>
+              {/* <br />
 							<Link
 								textColor="dktertiary"
 								style={styles.smallText}
@@ -107,18 +107,18 @@ export default class Presentation extends React.Component {
 								Video is available on YouTube:
 								https://youtu.be/1H8TO1fVuLY
 							</Link> */}
-						</Fill>
-					</Layout>
-				</Slide>
-        
+            </Fill>
+          </Layout>
+        </Slide>
+
         {/* About Me Slides */}
         <AboutMe />
         <Formidable />
 
         {/* 5 W's of Accessibility */}
-        <A11yIntro /> 
-        <WhatIsA11y /> 
-        <WhatIsDisability /> 
+        <A11yIntro />
+        <WhatIsA11y />
+        <WhatIsDisability />
         <TypesOfDisability />
         <Why />
         <WhyBusiness1 />
@@ -133,16 +133,13 @@ export default class Presentation extends React.Component {
 
         {/* Test Your Site */}
         <TestYourSiteIntro />
-        <Ksl />
-        <Utah />
-        <DesNews />
 
         {/* That seems like a lot. How do we break it down? */}
         <BreakdownIntro />
         <Checklists />
-				<StartNow />
-				<ByComponent />
-				<ByPage />
+        <StartNow />
+        <ByComponent />
+        <ByPage />
 
         {/* Conclusion */}
       </Deck>
--- presentation/testYourSite/index.js
+++ presentation/testYourSite/index.js
@@ -1,4 +1,1 @@
-export { default as TestYourSiteIntro } from './intro';
-export { default as Ksl } from './ksl';
-export { default as Utah } from './utah';
-export { default as DesNews } from './deseret-news';
+export { default as TestYourSiteIntro } from "./intro";
